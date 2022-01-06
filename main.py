3b4a2ff3e2ddb8d1fca505d2e1f039dac18ab01f
Emils Melkis <97155937+EmilsMelkis@users.noreply.github.com>
2022-01-06 06:57:26 +0000

    izpildits uzd

---
+++ main.py
@@ -0,0 +1,7 @@
+gar=float(input("ievadi kvadrata malas garumu: "))
+if gar>=5:
+  lauk=gar*gar
+  lauk2=(mala+5)*(mala+5)
+  print("procenti ir",100*(lauk2/lauk),"%")
+else:
+    print("ievadits nepareizs skaitlis")
